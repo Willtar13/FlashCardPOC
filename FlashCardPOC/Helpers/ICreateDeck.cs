775503770be84b88d6cdd195f8adb3db6353e129
Willtar13 <willsea80@gmail.com>
2019-03-22 14:10:03 -0600

    Not Sure Why I had to manually add these files

---
+++ FlashCardPOC/Helpers/ICreateDeck.cs
@@ -0,0 +1,14 @@
+﻿using FlashCardPOC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlashCardPOC.Helpers
+{
+    public interface ICreateDeck
+    {
+        List<FlashCard> CreateDeck(List<String> Categories);
+    }
+}
